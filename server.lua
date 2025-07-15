cefdcdde156d34fae77cbd4fe5ec2f73dd7a6d23
LeonS2k909 <leon2k909@outlook.com>
2025-07-16 00:38:47 +0100

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,25 +1,21 @@
--- server.lua for qb_assignpolicegear
-
 local QBCore = exports['qb-core']:GetCoreObject()
 
-RegisterNetEvent('qb-assignpolicegear:server:GivePoliceItems', function()
+RegisterServerEvent("qb-assignpolicegear:server:GivePoliceItems", function()
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
+    if not Player then return end
 
-    if Player and Player.PlayerData.job.name == "police" then
-        local items = {
-            { name = "weapon_appistol", amount = 1 },
-            { name = "weapon_stungun", amount = 1 },
-            { name = "handcuffs", amount = 1 },
-            { name = "radio", amount = 1 }
-        }
+    -- Define police gear
+    local items = {
+        { name = "weapon_stungun", amount = 1 },
+        { name = "handcuffs", amount = 1 },
+        { name = "weapon_appistol", amount = 1 },
+        { name = "radio", amount = 1 },
+    }
 
-        for _, item in pairs(items) do
-            if not Player.Functions.GetItemByName(item.name) then
-                Player.Functions.AddItem(item.name, item.amount)
-            end
+    for _, item in pairs(items) do
+        if not Player.Functions.GetItemByName(item.name) then
+            Player.Functions.AddItem(item.name, item.amount)
         end
-    else
-        print(("[qb-assignpolicegear] WARNING: Player %s tried to request police items without being police."):format(src))
     end
 end)
