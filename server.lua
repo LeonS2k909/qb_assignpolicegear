7fd4f8e7f144b062d4684dcde8f5ed389d37d90a
LeonS2k909 <leon2k909@outlook.com>
2025-07-16 00:41:21 +0100

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,3 +1,5 @@
+-- server.lua for qb_assignpolicegear
+
 local QBCore = exports['qb-core']:GetCoreObject()
 
 RegisterServerEvent("qb-assignpolicegear:server:GivePoliceItems", function()
@@ -5,7 +7,7 @@ RegisterServerEvent("qb-assignpolicegear:server:GivePoliceItems", function()
     local Player = QBCore.Functions.GetPlayer(src)
     if not Player then return end
 
-    -- Define police gear
+    -- Define police gear items
     local items = {
         { name = "weapon_stungun", amount = 1 },
         { name = "handcuffs", amount = 1 },
@@ -15,7 +17,14 @@ RegisterServerEvent("qb-assignpolicegear:server:GivePoliceItems", function()
 
     for _, item in pairs(items) do
         if not Player.Functions.GetItemByName(item.name) then
-            Player.Functions.AddItem(item.name, item.amount)
+            local added = Player.Functions.AddItem(item.name, item.amount)
+            if added then
+                TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items[item.name], "add")
+            else
+                print("[DEBUG] Failed to add item: " .. item.name)
+            end
+        else
+            print("[DEBUG] Player already has: " .. item.name)
         end
     end
 end)
